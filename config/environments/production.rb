ec7b1ace354544ad6bd1c240d171056db62763ad
Tolulope Oshinowo <toluooshy@gmail.com>
2020-09-02 16:20:33 -0400

    Updates to email bug in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,19 +93,20 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.default_url_options = { :host => 'toshinowo.herokuapp.com', :protocol => 'https' }  
+  config.action_mailer.default_url_options = { :host => 'toshinowo.herokuapp.com' }  
   config.action_mailer.delivery_method = :smtp  
   config.action_mailer.perform_deliveries = true  
   config.action_mailer.raise_delivery_errors = true  
   config.action_mailer.default :charset => "utf-8"  
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
+  config.action_mailer.smtp_settings = 
+  {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'toshinowo.herokuapp.com',
+    user_name:            ENV["GMAIL_EMAIL"],
+    password:             ENV["GMAIL_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true
   }
 
 end
