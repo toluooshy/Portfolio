f6b9fca4d005d952c2d63f31cb98e4b357ff6166
Tolulope Oshinowo <toluooshy@gmail.com>
2020-09-02 16:46:12 -0400

    Updates to email bug in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,8 +103,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'toshinowo.herokuapp.com',
-    user_name:            ENV["GMAIL_EMAIL"],
-    password:             ENV["GMAIL_PASSWORD"],
+    user_name:            'freshstudiosllc@gmail.com',
+    password:             'cr7juventus',
     authentication:       'plain',
     enable_starttls_auto: true
   }
