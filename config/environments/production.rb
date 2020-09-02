450af36a62c4a6b754cde534c5819c6ad7f9c683
Tolulope Oshinowo <toluooshy@gmail.com>
2020-09-02 15:02:59 -0400

    Updates to email bug in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,15 +97,16 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp  
   config.action_mailer.perform_deliveries = true  
   config.action_mailer.raise_delivery_errors = false  
-  ActionMailer::Base.smtp_settings = 
+  config.action_mailer.default :charset => "utf-8"  
+  config.action_mailer.smtp_settings = 
   {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV["GMAIL_EMAIL"],
+    password:             ENV["GMAIL_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true
   }
 
 end
