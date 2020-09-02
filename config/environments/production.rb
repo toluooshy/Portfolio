033f137b3dc5fc0a0c8cac71b371ed23919b2bcd
Tolulope Oshinowo <toluooshy@gmail.com>
2020-09-02 15:22:37 -0400

    Updates to email bug in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,14 +93,16 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors= true
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'http://toshinowo.herokuapp.com' }  
+  config.action_mailer.delivery_method = :smtp  
+  config.action_mailer.perform_deliveries = true  
+  config.action_mailer.raise_delivery_errors = false  
+  config.action_mailer.default :charset => "utf-8"  
   config.action_mailer.smtp_settings = 
   {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com',
+    domain:               'toshinowo.herokuapp.com',
     user_name:            ENV["GMAIL_EMAIL"],
     password:             ENV["GMAIL_PASSWORD"],
     authentication:       'plain',
